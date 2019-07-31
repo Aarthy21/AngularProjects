7e8ae511b7112f0b2707f80f0b10f89702f20324
kmukesht <kmukesh@skyessmail.com>
2019-07-31 18:51:52 +0530

    Final Source

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit} from '@angular/core';
+import { ChildComponent } from '../child/child.component';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-parent',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parent.component.css']
 })
 export class ParentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private msg:MessageService) { 
+    this.msg.userName.subscribe(uname=>{
+      this.userName=uname;
+      console.log("USERNAME======>"+this.userName)
+    })
   }
+message:string;
+userName:string="Aarthy"
+ngOnInit(){
+  this.msg.currentMessage.subscribe(message => this.message =
+    message);
+}
 
+UpdateVal(uname){
+  
+  this.msg.userName.next(uname.value);
+}
+
+  // receiveMsg($event){
+  //   this.message=$event;
+  //   console.log("message=>"+this.message);
+  // }
 }
